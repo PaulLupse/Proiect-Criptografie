e2769f4825a9514e9b5783e658159232cc9868e8
PaulLupse <Lupse.Io.Paul@student.utcluj.ro>
2025-04-16 08:31:34 +0300

    Adaugat script-uri pentru algoritmii de criptare

---
+++ Pachete/Algoritmi/polybius.py
@@ -0,0 +1,16 @@
+def polybius(msg, alfabet, latura):
+    ...
+
+def ADFGX(msg, alfabet, cheie):
+    ...
+
+def ADFGVX(msg, alfabet, cheie):
+    ...
+
+def bifid(msg, alfabet, latura):
+    ...
+
+def vignere(msg, cheie):
+    ...
+
+
